955cd7472244608db2d0914ab05ae31f7dbe3473
Megrax <geronimooo@foxmail.com>
2022-09-15 11:02:24 +0800

    chore: test CI

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import MainLayout from "@/layouts/main";
+import { union } from "lodash";
 
 const Home: NextPage = () => {
+  console.log(union([1, 2, 3], [2, 3, 4, 5, 1, 2]));
   return (
     <MainLayout title="The Ultimate AWS RDS and Google Cloud SQL Instance Pricing Sheet">
       <Head>
